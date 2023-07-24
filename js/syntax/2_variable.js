a297aadf278e41a154b977eaa5570720a563871a
sungjin-han <sjhan228@gmail.com>
2023-07-24 22:09:52 +0900

    NIPA DAY6 과제

--- js/syntax/2_variable.js
+++ js/syntax/2_variable.js
@@ -6,8 +6,12 @@ var date;
 
 // let 변수
 let year = 2023;
-var year = 2023;
+//var year = 2023;
+// 이미 let으로 선언함
 
 // const 변수
 const day = 10;
-const day = 12;
+//const day = 12;
+// 이미 const로 선언함
+
+// 최대한 var을 쓰지말고 가급적 let, const로 쓰는게 좋다
